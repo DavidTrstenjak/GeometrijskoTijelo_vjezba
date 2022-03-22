92d70c753f11c4b03e88d7bbe6eb247ac62b6afa
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-03-22 13:16:45 +0100

    Add files via upload

---
+++ Vjezba_NebeskoTijelo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba_NebeskoTijelo
+{
+    class GeometrijskoTijelo
+    {
+        protected double Volumen;
+        protected double Oplosje;
+        public static int BrojacTijela;
+
+        public GeometrijskoTijelo()
+        {
+            BrojacTijela++;
+        }
+    }
+
+    class Kugla : GeometrijskoTijelo
+    {
+        
+    }
+
+    class Kvadar : GeometrijskoTijelo
+    {
+
+    }
+
+    class Kocka : Kvadar
+    {
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Kugla x = new Kugla();
+             Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+             Kvadar y = new Kvadar();
+             Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+             Kocka z = new Kocka();
+             Console.WriteLine(GeometrijskoTijelo.BrojacTijela);
+
+
+
+            Console.ReadKey();
+        }
+
+            
+        }
+    }
+
